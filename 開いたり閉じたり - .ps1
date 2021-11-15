20ff64ada7a00d3b957d7227072b9fcb429c016a
momiji910 <94339356+momiji910@users.noreply.github.com>
2021-11-15 13:27:24 +0900

    Add files via upload

---
+++ 開いたり閉じたり - .ps1
@@ -0,0 +1,26 @@
+﻿#何分間動かすか指定
+#60だったら1時間
+$x = 60
+$i = 1
+
+write "start!"
+while ($i -le $x) {
+    #メモ帳を開く
+    Start-Sleep 59;
+
+    #開くメモ帳のディレクトリを記載
+    notepad.exe C\Temp123.txt
+
+    #メモ帳を終了
+    #＜注意＞メモ帳で開かれているものはすべて閉じます
+    $pro = Get-process notepad
+    Start-Sleep 1;
+    if ( $pro -ne $null ) {
+    Stop-process -name notepad
+    }
+
+    $x -=1
+
+    write "残り $x 分"
+}
+write "Finish!"
